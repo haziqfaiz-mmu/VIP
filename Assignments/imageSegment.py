6d41ad36f3efbd823a689a5fbdd8840e52e2b60d
Haziq Faiz <1201302740@student.mmu.edu.my>
2023-12-26 19:12:57 +0800

    assignment

---
+++ Assignments/imageSegment.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+imageSegment.py
+
+YOUR WORKING FUNCTION
+
+"""
+import cv2
+import numpy as np
+
+
+input_dir = 'dataset/test'
+output_dir = 'dataset/output'
+
+# you are allowed to import other Python packages above
+##########################
+def segmentImage(img):
+    # Inputs
+    # img: Input image, a 3D numpy array of row*col*3 in BGR format
+    #
+    # Output
+    # outImg: segmentation image
+    #
+    #########################################################################
+    # ADD YOUR CODE BELOW THIS LINE
+   
+    
+    
+    
+    
+    
+    
+    
+    # END OF YOUR CODE
+    #########################################################################
+    return outImg
